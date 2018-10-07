411fe13c596799b2da615c713d5e08f0d0d4a948
Well Lima <contato@criacaodesite.biz>
2018-10-07 01:49:17 -0300

    Refinamento do retorno 400 e também no teste unitário

--- src/test/java/app/AppControllerIT.java
+++ src/test/java/app/AppControllerIT.java
@@ -1,17 +1,33 @@
 package app;
 
 import static com.jayway.restassured.RestAssured.when;
+import static org.junit.Assert.assertEquals;
+
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.desafioapp.data.CSVToDataBaseH2;
+
 @WebAppConfiguration
 public class AppControllerIT {	
 	
 	// Cenário esperado
-	// Obs.: Precisa parar a aplicação e iniciar novamente para que se tenha como excluir o filme, 
-	//       pois caso contrário dará falha na hora de testar
+	// Obs.: Precisa parar a aplicação e iniciar novamente assim o banco será populado para realizar o teste de deleção, 
+	//       pois caso contrário dará falha na hora de testar pela 2º vez
+	
+	@Test
+    public void canCSVToDataBaseH2() {
+		// Loading CSV file to H2 Memory DataBase
+		CSVToDataBaseH2 loadFile = new CSVToDataBaseH2();
+		int loadCSV = 0;
+		if(loadFile.loadCSVToH2()) {
+			loadCSV = 1;
+		}
+		System.out.println("Status CSV TO H2: " + loadCSV);	
+		assertEquals(1, loadCSV);
+	}	
 	
 	@Test
     public void canFetchMoviesForYear() {
@@ -78,7 +94,6 @@ public class AppControllerIT {
                 statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED).
                 body("codreturn", Matchers.containsString("00405"));               
     }		
-	
 	
 	@Test
     public void canNotExistPath1() {
